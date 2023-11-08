44fb0ecb9a6e735c00954f9695749a2436f04eb2
johnhca <srcascudo.in@gmail.com>
2023-11-08 17:20:40 -0300

    @text(max, min)

--- validator.py
+++ validator.py
@@ -12,8 +12,8 @@ def throw_error(msg):
         logging.error(Fore.RED + err + Fore.RESET)
     logging.error(Fore.RED + ' ' + msg + Fore.RESET)
 
-def max_char(value: int) -> object:
-    """Determina o tamanho máximo da str de entrada de uma função."""
+def text(max: int = 255, min: int = 0) -> object:
+    """Determina o tamanho máximo e mínimo da str de entrada de uma função."""
     def execute(func: object) -> object:
         @wraps(func)
         def call(*args) -> object:
@@ -21,25 +21,31 @@ def max_char(value: int) -> object:
                 
                 param = args[0] if func.__name__ == func.__qualname__ else args[1]
 
-                if not isinstance(value, int) or isinstance(value, bool):
+                if (not isinstance(max, int) or isinstance(max, bool)) or (not isinstance(min, int) or isinstance(min, bool)):
                     raise TypeError(1)
                 
                 if not isinstance(param, str):
                     raise TypeError(2)
 
-                if len(param) > value:
-                    raise ValueError
+                if len(param) > max:
+                    raise ValueError(1)
+                
+                if len(param) < min:
+                    raise ValueError(2)
                 
                 return func(*args)
         
             except TypeError as e:
                 if e.args[0] == 1:
-                    throw_error(f'A função {func.__name__}() está utilizando o decorator @max_len e informando um tipo diferente de int(): {type(value)}')
+                    throw_error(f'A função {func.__name__}() está utilizando o decorator @text e informando um tipo diferente de int():\nmax: {type(max)} min: {type(min)}')
                 if e.args[0] == 2:
                     throw_error(f'Não é possível atribuir {type(param)} para {func.__name__}()')
 
-            except ValueError:
-                throw_error(f'A função {func.__name__}() suporta até {value} caracteres, e você está informando {len(param)}.')
+            except ValueError as e:
+                if e.args[0] == 1:
+                    throw_error(f'A função {func.__name__}() suporta no máximo {max} caracteres, e você está informando {len(param)}.')
+                if e.args[0] == 2:
+                    throw_error(f'A função {func.__name__}() suporta no minimo {min} caracteres, e você está informando {len(param)}.')
             
             return
         return call
