09bdbca14ad0f12274b96c03dab874a9db1b7d71
johnhca <srcascudo.in@gmail.com>
2023-11-08 16:50:13 -0300

    max_char

--- validator.py
+++ validator.py
@@ -19,13 +19,15 @@ def max_char(value: int) -> object:
         def call(*args) -> object:
             try:
                 
+                param = args[0] if func.__name__ == func.__qualname__ else args[1]
+
                 if not isinstance(value, int) or isinstance(value, bool):
                     raise TypeError(1)
                 
-                if not isinstance(args[1], str):
+                if not isinstance(param, str):
                     raise TypeError(2)
 
-                if len(args[1]) > value:
+                if len(param) > value:
                     raise ValueError
                 
                 return func(*args)
@@ -34,10 +36,10 @@ def max_char(value: int) -> object:
                 if e.args[0] == 1:
                     throw_error(f'A função {func.__name__}() está utilizando o decorator @max_len e informando um tipo diferente de int(): {type(value)}')
                 if e.args[0] == 2:
-                    throw_error(f'Não é possível atribuir {type(args[1])} para {func.__name__}()')
+                    throw_error(f'Não é possível atribuir {type(param)} para {func.__name__}()')
 
             except ValueError:
-                throw_error(f'A função {func.__name__}() suporta até {value} caracteres, e você está informando {len(args[1])}.')
+                throw_error(f'A função {func.__name__}() suporta até {value} caracteres, e você está informando {len(param)}.')
             
             return
         return call
